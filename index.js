31179e0e70bda77a65dfa8f46daac8a6f583dd43
joslough <joslough@pulicisgroupe.net>
2022-07-27 13:31:57 +0100

    Change port to static

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import * as http from 'http';
 
+const port = 3000;
+
 const server = http.createServer((req, res) => {
     res.write(`<button onclick=${"console.log('Click!')"}>Welcome to NODE app!</button>`);
     res.end();
 });
 
-server.listen(process.env.PORT);
+server.listen(port);
 
 /*
 OS Linux
